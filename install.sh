6290de76a777acf94f28546ca2b392648a32e150
Happensit <antonybizov@gmail.com>
2015-09-16 18:55:36 +0300

    Add Drupal support

--- install.sh
+++ install.sh
@@ -27,11 +27,13 @@ curl -Lo /usr/local/etc/nginx/common/drupal https://raw.githubusercontent.com/Ha
 # Download Virtual Hosts.
 curl -Lo /usr/local/etc/nginx/sites-available/default https://raw.githubusercontent.com/Happensit/Homebrew-getlooky/master/conf/nginx/sites-available/default
 curl -Lo /usr/local/etc/nginx/sites-available/drupal.dev https://raw.githubusercontent.com/Happensit/Homebrew-getlooky/master/conf/nginx/sites-available/drupal.dev
-curl -Lo /usr/local/etc/nginx/sites-available/drupal.dev https://raw.githubusercontent.com/Happensit/Homebrew-getlooky/master/conf/nginx/sites-available/getlooky.dev
+curl -Lo /usr/local/etc/nginx/sites-available/getlooky.dev https://raw.githubusercontent.com/Happensit/Homebrew-getlooky/master/conf/nginx/sites-available/getlooky.dev
+curl -Lo /usr/local/etc/nginx/sites-available/xhprof.dev https://raw.githubusercontent.com/Happensit/Homebrew-getlooky/master/conf/nginx/sites-available/xhprof.dev
 
 ln -s /usr/local/etc/nginx/sites-available/default /usr/local/etc/nginx/sites-enabled/default
 ln -s /usr/local/etc/nginx/sites-available/getlooky.dev /usr/local/etc/nginx/sites-enabled/getlooky.dev
-
+ln -s /usr/local/etc/nginx/sites-available/drupal.dev /usr/local/etc/nginx/sites-enabled/drupal.dev
+ln -s /usr/local/etc/nginx/sites-available/xhprof.dev /usr/local/etc/nginx/sites-enabled/xhprof.dev
 
 # Create folder for logs.
 rm -rf /usr/local/var/log/{fpm,nginx}
@@ -62,6 +64,10 @@ echo 'xhprof.output_dir=/tmp/xhprof' >>  /usr/local/etc/php/5.6/conf.d/ext-xhpro
 curl -Lo /usr/local/etc/nginx/sites-available/xhprof.dev https://raw.githubusercontent.com/Happensit/Homebrew-getlooky/master/conf/nginx/sites-available/xhprof.dev
 ln -s /usr/local/etc/nginx/sites-available/xhprof.dev /usr/local/etc/nginx/sites-enabled/xhprof.dev
 sudo echo '127.0.0.1 xhprof.dev' >>  /etc/hosts
+sudo echo '127.0.0.1 getlooky.dev' >>  /etc/hosts
+sudo echo '127.0.0.1 drupal.dev' >>  /etc/hosts
+sudo echo '127.0.0.1 xhprof.dev' >>  /etc/hosts
+
 
 echo '✩✩✩✩ Drush ✩✩✩✩'
 brew install drush
